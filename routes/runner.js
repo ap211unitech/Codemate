9d8b9fb7be60aae6f38c95f4723a785b7527700a
AP <porwal.1@iitj.ac.in>
2022-07-28 20:21:36 +0530

    Changed to latest features

--- routes/runner.js
+++ routes/runner.js
@@ -4,11 +4,17 @@ const request = require("request");
 router.post('/', (req, res) => {
     const url = "https://api.jdoodle.com/v1/execute";
 
+    const vIndex = {
+        'cpp': '5',
+        'python3': '4',
+        'c': '5'
+    }
+
     const program = {
         script: req.body.code,
         stdin: req.body.input,
         language: req.body.lang,
-        versionIndex: "0",
+        versionIndex: vIndex[req.body.lang],
         clientId: "5ce99d471629ae027355c445ccd8bb8f",
         clientSecret: "fef64ae65a4b1a6b9234e10b18f7ef88119b5874bcf00f409bbdc973d7d0a073",
     };
